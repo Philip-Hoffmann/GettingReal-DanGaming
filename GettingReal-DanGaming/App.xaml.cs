a28167d001ed54ccca114ee36e73e91509465b73
Philip-Hoffmann <Philip.t.hoffmann@gmail.com>
2025-11-11 12:36:14 +0100

    Add project files.

---
+++ GettingReal-DanGaming/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GettingReal_DanGaming
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
