f77e2ccf5497d478063d4b673c981739e07ad5cf
Sara Hougaard <saraolsen20@gmail.com>
2025-11-24 13:28:48 +0100

    Product tilføjet

--- GettingReal-DanGaming/Models/Product.cs
+++ GettingReal-DanGaming/Models/Product.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace GettingReal_DanGaming.Models
 {
-    internal class Product
+    public class Products
     {
-        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public double Price { get; set; }
+        public integer Quantity { get; set; }
+        public integer MinimumQuantity { get; set; }
+        private string _dateOfReceipt;
+
+        public string DateOfRecipt
+        {
+            get { return _dateOfReceipt; }
+            set { _dateOfReceipt = value; }
+        }
+
     }
 }
