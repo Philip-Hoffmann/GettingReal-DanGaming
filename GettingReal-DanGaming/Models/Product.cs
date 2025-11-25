b62f0d4c98d149df57abd26fbf2c9317abf0d557
Mathias Sørensen <mathiassrensen514@gmail.com>
2025-11-25 13:52:18 +0100

    ids and repo

--- GettingReal-DanGaming/Models/Product.cs
+++ GettingReal-DanGaming/Models/Product.cs
@@ -8,34 +8,30 @@ namespace GettingReal_DanGaming.Models
 {
     public class Product
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Brand { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
         public int MinimumQuantity { get; set; }
-        private string _dateOfReceipt;
+        public string DateOfRecipt { get; set; }
 
-        public string DateOfRecipt
-        {
-            get { return _dateOfReceipt; }
-            set { _dateOfReceipt = value; }
-        }
+        public int CategoryId { get; set; }
+        public int SubcategoryId { get; set; }
 
-        public int addQuantity(Product product)
+        public int AddQuantity(Product product)
         {
             return 0;
         }
 
-        public int removeQuantity(Product product)
+        public int RemoveQuantity(Product product)
         {
             return 0;
         }
 
-        public void editProduct(Product product)
+        public void EditProduct(Product product)
         {
-
         }
-
     }
 }
