483f44c19a91fee6b7947c405f7e8eca360dc072
Philip-Hoffmann <Philip.t.hoffmann@gmail.com>
2025-11-25 10:48:45 +0100

    Små ændringer

--- GettingReal-DanGaming/Models/Warehouse.cs
+++ GettingReal-DanGaming/Models/Warehouse.cs
@@ -8,26 +8,16 @@ namespace GettingReal_DanGaming.Models
 {
     public class Warehouse
     {
-        private string _name;
-        private string _location;
+        public string Name { get; set; }
+        public string Location { get; set; }
 
 
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
-        public string Location
-        {
-            get { return _location; }
-            set { _location = value; }
-        }
         public void AddCategory(Category category)
         {
             // Method intentionally left empty.
         }
 
-        public static void Remove(Category category)
+        public void Remove(Category category)
         {
             // Method intentionally left empty.
         }
