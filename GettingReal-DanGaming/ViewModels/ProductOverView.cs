cc20da2f8744844b7edc71e3b6c49eb33514263e
Philip-Hoffmann <Philip.t.hoffmann@gmail.com>
2025-11-25 13:59:30 +0100

    UI 3.0 + ProductOverView

---
+++ GettingReal-DanGaming/ViewModels/ProductOverView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal_DanGaming.ViewModels
+{
+    // Define ProductNameList class since it does not exist in the current context
+    public class ProductNameList
+    {
+        public string Name { get; set; }
+    }
+
+    public class ProductOverView
+    {
+        public string ProductName { get; set; } = "";
+        public string ProductBrand { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductSubCategory { get; set; }
+        public int ProductQuantity { get; set; }
+
+    }
+}
