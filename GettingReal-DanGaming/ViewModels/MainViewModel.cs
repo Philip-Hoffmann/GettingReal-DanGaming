2b215c1b1c117739bdfe602843046476b7e4c036
Mathias Sørensen <mathiassrensen514@gmail.com>
2025-11-26 10:32:21 +0100

    produkt søgning

--- GettingReal-DanGaming/ViewModels/MainViewModel.cs
+++ GettingReal-DanGaming/ViewModels/MainViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using GettingReal_DanGaming.Models;
 
 namespace GettingReal_DanGaming.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
         private CategoryRepository categoryRepo;
         private ProductRepository productRepo;
@@ -20,6 +21,31 @@ namespace GettingReal_DanGaming.ViewModels
             {
                 ProductsVM.Add(new ProductViewModel(product, categoryRepo));
             });
+        }
+
+        private string _productSearch;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string ProductSearch 
+        { 
+            get 
+            {
+                return _productSearch;
+            }
+            
+            set
+            {
+                _productSearch = value;
+                ProductsVM.Clear();
+                productRepo.GetAll().ForEach(product =>
+                {
+                    if (product.Name.StartsWith(_productSearch, StringComparison.OrdinalIgnoreCase))
+                    {
+                        ProductsVM.Add(new ProductViewModel(product, categoryRepo));
+                    }
+                });
+            }
         }
     }
 }
