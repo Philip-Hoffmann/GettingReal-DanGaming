9b896f9b9f3e132a7c053250276821c776b265fd
Mathias Sørensen <mathiassrensen514@gmail.com>
2025-11-26 11:20:14 +0100

    views

---
+++ GettingReal-DanGaming/ViewModels/AddProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GettingReal_DanGaming.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal_DanGaming.ViewModels
+{
+    public class AddProductViewModel
+    {
+        private CategoryRepository categoryRepo;
+        private ProductRepository productRepo;
+
+        public AddProductViewModel()
+        {
+            categoryRepo = new CategoryRepository();
+            productRepo = new ProductRepository();
+        }
+    }
+}
